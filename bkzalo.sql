e6f957800f86f20238d71f9cbd3a69d71447a532
thaidat01222 <thaidat01222@gmail.com>
2021-06-22 10:17:23 +0700

    sql

--- bkzalo.sql
+++ bkzalo.sql
@@ -1,42 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 4.5.1
--- http://www.phpmyadmin.net
---
--- Host: 127.0.0.1
--- Generation Time: Jun 08, 2021 at 04:58 AM
--- Server version: 10.1.13-MariaDB
--- PHP Version: 7.0.6
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
 -- Database: `bkzalo`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `message`
---
-
-CREATE TABLE `message` (
-  `id` int(11) NOT NULL,
-  `fromID` varchar(25) NOT NULL,
-  `toID` varchar(25) NOT NULL,
-  `content` text NOT NULL,
-  `image` varchar(50) NOT NULL,
-  `contentType` varchar(10) NOT NULL,
-  `seenTime` date NOT NULL,
-  `sentTime` date NOT NULL,
-  `status` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -48,40 +12,41 @@ CREATE TABLE `users` (
   `username` varchar(55) NOT NULL,
   `password` varchar(55) NOT NULL,
   `email` varchar(55) NOT NULL,
-  `avatar` varchar(255) NOT NULL,
-  `fullName` varchar(100) NOT NULL,
+  `avatar` varchar(255) NOT NULL DEFAULT '/image/avatar/',
+  `fullName` varchar(100) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL,
   `phoneNumber` varchar(14) NOT NULL,
   `id` int(11) NOT NULL,
   `isLogin` tinyint(1) NOT NULL DEFAULT '0',
-  `expireTime` date NOT NULL
+  `logoutTime` datetime NOT NULL,
+  `age` int(2) NOT NULL,
+  `synopsis` varchar(255) CHARACTER SET utf8 COLLATE utf8_vietnamese_ci NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`username`, `password`, `email`, `avatar`, `fullName`, `phoneNumber`, `id`, `isLogin`, `expireTime`) VALUES
-('admin', 'admin', 'admin@gmail.com', '', 'admin', '', 1, 0, '0000-00-00'),
-('client', 'client', 'client@gmail.com', '', 'client', '', 2, 0, '0000-00-00'),
-('duydat1', 'duydat', 'duydat1@gmail.com', '', 'duy dat', '', 3, 0, '0000-00-00'),
-('duydat2', 'duydat', 'duydat2@gmail.com', '', 'duydat', '', 4, 0, '0000-00-00'),
-('duydat3', 'duydat', 'duydat3@gmail.com', '', 'duydat', '', 5, 0, '0000-00-00'),
-('Percy Jackson', 'hihihaha', 'percyusjasson@gmail.com', '', '', '', 6, 0, '0000-00-00'),
-('phanhien1', 'phanhien', 'phanhien1@gmail.com', '', 'phanhien', '', 7, 0, '0000-00-00'),
-('phanhien2', 'phanhien', 'phanhien2@gmail.com', '', 'phanhien', '', 8, 0, '0000-00-00'),
-('phanhien203', 'hiendangyeu', 'phanhien203@gmail.com', '', 'phan thi thu hien', '0764622582', 9, 1, '0000-00-00'),
-('phanhien3', 'phanhien', 'phanhien3@gmail.com', '', 'phanhien', '', 10, 0, '0000-00-00'),
-('thaidat01222', 'quang123', 'thaidat01222@gmail.com', '', 'thai duy dat', '0762865927', 11, 1, '0000-00-00');
+INSERT INTO `users` (`username`, `password`, `email`, `avatar`, `fullName`, `phoneNumber`, `id`, `isLogin`, `logoutTime`, `age`, `synopsis`) VALUES
+('Dora', '1', 'c@gmail.com', '/image/avatar/15.jpg', 'Doraemon', '', 15, 0, '2021-06-16 13:58:26', 0, 'Nobita-kun'),
+('hoaithan', 'hoaithan', 'hoaithan@gmail.com', '/image/avatar/1.jpg', 'Trần Hoài Thân', '0161616165', 1, 0, '2021-06-16 10:54:24', 22, 'The Cassette!!!'),
+('minhduc', 'minhduc', 'minhduc@gmail.com', '/image/avatar/5.jpg', 'Lê Minh Đức', '019191919', 5, 0, '2021-06-16 11:00:04', 21, 'Hãy tử tế, vì tất cả những người bạn gặp đều đang chiến đấu một trận chiến khó khăn của họ'),
+('Naruto', '1', 'a@gmail.com', '/image/avatar/13.jpg', 'Uzumaki Bố Của Boruto', '016161616', 13, 1, '0000-00-00 00:00:00', 22, 'I am going to become Hokage!!!!!!!!!!!!!!!!'),
+('ngochoa', 'ngochoa', 'ngochoa@gmail.com', '/image/avatar/7.jpg', 'NTN Hoa', '012121212', 7, 0, '2021-06-16 11:01:04', 222, 'Cái gì cũng biết chỉ có biết điều là không'),
+('nhungoc', 'nhungoc', 'nhungoc@gmail.com', '/image/avatar/6.jpg', 'Hồ Thị Như Ngọc', '0101010101', 6, 0, '2021-06-16 11:00:50', 11, 'Hãy sống mỗi ngày như thể đó là ngày cuối cùng của bạn'),
+('Nobita', '1', 'b@gmail.com', '/image/avatar/14.jpg', 'Nobi Nobita', '017171717', 14, 0, '2021-06-19 15:07:09', 87, 'Doraemonnnnnn huhuhu!!'),
+('phanhien203', 'phanhien203', 'phanhien203@gmail.com', '/image/avatar/9.jpg', 'Phan Thị Thu Hiền', '0764622582', 9, 1, '0000-00-00 00:00:00', 22, 'Never give up <3 '),
+('phuonghien', 'phuonghien', 'phuonghien@gmail.com', '/image/avatar/10.jpg', 'Huỳnh Thị Phương Hiền', '013131313', 10, 0, '2021-06-16 15:40:52', 23, 'Hìn Hìn'),
+('quocan', 'quocan', 'quocan@gmail.com', '/image/avatar/3.jpg', 'Vũ Quốc An', '1414141414', 3, 0, '2021-06-20 13:31:06', 33, 'Nếu bạn đối xử với một con chó tốt như một con người, thì nó sẽ đối xử với bạn như một con chó'),
+('sasuke', '1', 'd@gmail.com', '/image/avatar/16.jpg', 'Uchiha Papasuke', '0124586224', 16, 0, '2021-06-19 14:43:13', 40, 'Itachi ni-san, I miss you'),
+('thaidat01222', 'quang123', 'thaidat01222@gmail.com', '/image/avatar/11.jpg', 'Thái Duy Đạt', '0762865927', 11, 1, '0000-00-00 00:00:00', 22, 'Người yêu tên Hiền'),
+('thaihiep', 'thaihiep', 'thaihiep@gmail.com', '/image/avatar/4.jpg', 'Cao Văn Thái Hiệp', '018181818', 4, 0, '2021-06-16 10:59:00', 25, 'Hạnh phúc đạt được khi bạn ngừng chờ đợi điều đó xảy ra và thực hiện các bước để biến nó thành hiện thực.'),
+('tronganh', 'tronganh', 'tronganh@gmail.com', '/image/avatar/2.jpg', 'Đặng Lê Trọng Anh', '0151515151', 2, 0, '2021-06-16 10:29:26', 23, 'Take it easy!'),
+('vanha', 'vanha', 'vanha@gmail.com', '/image/avatar/8.jpg', 'Nguyễn Văn Hà', '0131141414', 8, 0, '2021-06-16 11:08:00', 17, 'Nói ít đi, đọc nhiều hơn'),
+('viethoang', 'viethoang', 'viethoang@gmail.com', '/image/avatar/12.jpg', 'Bùi Tuấn Việt Hoàng', '015151515', 12, 0, '2021-06-16 11:12:08', 99, 'Rồi một ngày em sẽ nhận ra, những đóa hoa vẫn nở dù em tưới chúng bằng nước mắt của chính mình.');
 
 --
 -- Indexes for dumped tables
 --
-
---
--- Indexes for table `message`
---
-ALTER TABLE `message`
-  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -96,15 +61,10 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `message`
---
-ALTER TABLE `message`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
